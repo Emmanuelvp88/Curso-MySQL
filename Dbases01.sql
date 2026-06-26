6248867b885d723f509868bdc6a8cd0271106843
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2026-06-26 14:34:59 -0600

    Insertando mas datos en la tabla de animales

--- Dbases01.sql
+++ Dbases01.sql
@@ -12,31 +12,58 @@ PRIMARY KEY (id) -- Tenemos que indicar que parametro seria nuestra llave primar
 
 -- Antes de cualquier insercion de datos en una tabla, necesitamos indicar como se va hacer el conteo de cada insercion, haciendo autoincrementable la llave primaria "id"
 -- con la siguiente linea modificamos la tabla 'animales' y seleccionamos el parametro que queremos que sea Autoincrementable en este caso 'id' .
-ALTER TABLE animales MODIFY COLUMN id int auto_increment;
+ALTER TABLE animales 
+MODIFY COLUMN id int auto_increment PRIMARY KEY;
 
 -- Agregar nuevas columnas a una tabla existente
 ALTER TABLE animales
-ADD COLUMN id varchar (50)
+ADD COLUMN nombre varchar (50)
 FIRST;
 
 -- Con esto eliminamos columnas de una tabla
 ALTER TABLE animales
 DROP COLUMN nombre;
 
+UPDATE animales
+SET id =  3
+WHERE id = 4;
+
+
 -- Muestra todo lo que contiene la tabla "animales"
 SELECT * FROM animales;
 
--- Para eliminar un resgistro nediante el id
+
+
+-- Para eliminar un resgistro Mediante el id con limite
 DELETE FROM animales
-WHERE id = null;
+WHERE id IS NULL
+LIMIT 1;
 
 -- Insercion de datos en tablas
-INSERT INTO  animales (nombre, tipo, estado) VALUES ( 'Miklo','gato', 'feliz');
+INSERT INTO  animales (id, nombre, tipo, estado)
+ VALUES (1, 'Miklo','gato', 'feliz');
+ 
+INSERT INTO animales (nombre, tipo, estado)
+ VALUES ('Whalter','Perro','Mongolo');
+ 
+ INSERT INTO animales (nombre, tipo, estado) 
+ VALUES ('chikis', 'Perro','Muy feliz');
+
+INSERT INTO animales (nombre, tipo, estado)
+VALUES ('la peque', 'Perro','enojona');
+
+INSERT INTO animales (nombre, tipo, estado)
+VALUES('canelita', 'Perro', 'tranquila');
+
+
+
+
+
 
 -- Te mustra el ejemplo de como se debe crear una tabla correctamente
 SHOW CREATE TABLE animales;
 
--- te arroja este ejemplo, desde un principio con el parametro 'id' autoincrementable, lo puedes copiar y pegar par la creacion de una tabla nueva
+-- Y este es el ejemplo que te arroja el comando anterior, desde un principio con el parametro 'id' autoincrementable, lo puedes copiar y pegar par la creacion de una tabla nueva
 -- solo quitar las comillas del principio y fin, y poner nuevo nombre a la tabla.
 'CREATE TABLE `animales` (
   `id` int NOT NULL AUTO_INCREMENT,
